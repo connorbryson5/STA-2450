1059e21849ca3e09e63489289d743549f1562afd
AFriedlander06 <aaron_friedlander1@baylor.edu>
2022-11-16 14:45:03 -0600

    Update HW 5.py

--- HW 5.py
+++ HW 5.py
@@ -16,14 +16,22 @@ def bootstrap_sample(data):
 def trimmed_mean(data, area):
     bootstrap = bootstrap_sample(data)
     bootstrap.sort()
-    sorted_bootstrap = bootstrap
-    trimmed_mean = stats.trim_mean(sorted_bootstrap, area)
-    
+    trimmed_mean = stats.trim_mean(bootstrap, area)
     return trimmed_mean
+
+def bootstrap_trimmed_mean(data, area, simulations):
+    bootstrapMeanList = []
+    for i in range (1, simulations):
+        bootstrapMean = trimmed_mean(data, area)
+        bootstrapMeanList.append(bootstrapMean)
+    bootstrapMeanList.sort()
+    trimmedbootstrapMean = stats.trim_mean(bootstrapMeanList, area)
+    return trimmedbootstrapMean
     
     
 data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 18, 50, 59, 1009, 38381, 909]
-trimmed_mean(data, .53)
+trimmed_mean(data, 0.1)
+bootstrap_trimmed_mean(data, 0.1, 10)
 
 
     
